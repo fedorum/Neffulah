c844a89fe141daec3da96fa0dbd046d7e0744d1b
fedorum <isaaccheah@outlook.com>
2025-12-04 23:31:04 -0400

    Fixed product page alignment and resizing

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -4,6 +4,14 @@ function Search() {
     return (
         <div id='filterDiv'>
             <p id='filter'>Filter</p>
+            <p>Category 1</p>
+            <p>Category 2</p>
+            <ul id='filterList'>
+                {/* retrieve number of 'categories' from user */}
+                <li>Sub-category 1</li>
+                <li>Sub-category 2</li>
+            </ul>
+            <p>Category 3</p>
         </div>
     );
 }
