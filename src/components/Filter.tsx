9341270a55953bef4aba599a551863d26ef1f9c6
fedorum <isaaccheah@outlook.com>
2025-12-06 23:25:25 +0700

    Adjusted filter column and list elements

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -4,14 +4,14 @@ function Search() {
     return (
         <div id='filterDiv'>
             <p id='filter'>Filter</p>
-            <p>Category 1</p>
-            <p>Category 2</p>
+            <p className='category'>Category 1</p>
+            <p className='category'>Category 2</p>
             <ul id='filterList'>
-                {/* retrieve number of 'categories' from user */}
-                <li>Sub-category 1</li>
-                <li>Sub-category 2</li>
+                {/* retrieve category data from uploaded images from user */}
+                <li className='subCategory'>Sub-category 1</li>
+                <li className='subCategory'>Sub-category 2</li>
             </ul>
-            <p>Category 3</p>
+            <p className='category'>Category 3</p>
         </div>
     );
 }
