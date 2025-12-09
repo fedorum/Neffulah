0f9f2d4e61ea24c8b1ef624b2b48aecdc7ee3a30
fedorum <isaaccheah@outlook.com>
2025-12-09 23:40:48 +0800

    Added a search button for the search input

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -4,6 +4,7 @@ function Search() {
     return (
         <div id='searchDiv'>
             <input id="searchInput" placeholder='Search by Product Code'></input>
+            <button id='searchButton'><i className="fas fa-search search-icon" id='searchIcon'></i></button>
         </div>
     );
 }
