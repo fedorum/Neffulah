0f355f95ef6bf26107f82bd044dc835a88f7f9ee
fedorum <isaaccheah@outlook.com>
2025-12-10 22:53:32 +0800

    Finalised rough design for products page

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -3,7 +3,7 @@ import '../App.css';
 function Search() {
     return (
         <div id='searchDiv'>
-            <input id="searchInput" placeholder='Search by Product Code'></input>
+            <input id="searchInput" spellCheck="false" placeholder='Search by Product Code'></input>
             <button id='searchButton'><i className="fas fa-search search-icon" id='searchIcon'></i></button>
         </div>
     );
