9fbc3dde4c59409d0ff1c0ba9b9e2638ce83615c
fedorum <isaaccheah@outlook.com>
2026-01-01 23:43:39 +0800

    Re-initialised server structure for file directory reading

--- api/server.js
+++ api/server.js
@@ -1,1 +1,25 @@
 // SERVER CODE FOR DIRECTORY READING
+import * as fs from 'fs';
+import * as path from 'path';
+
+export default function handler(req, res) {
+    if (req.method !== 'GET') {
+        res.status(405).json({ error: 'Request method not allowed'});
+        return;
+    }
+
+    const productsDir = path.join(process.cwd(), 'api', 'products');
+
+    fs.readdir(productsDir, { withFileTypes: true }, (err, files) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+
+        const folders = files
+            .filter(file => file.isDirectory())
+            .map(file => file.name);
+
+        res.status(200).json({ files: folders });
+    });
+}
--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import '../App.css';
-// import Card from './Card';
+import Card from './Card';
 
 function Grid() {
     // const [files, setFiles] = useState([]);
@@ -10,13 +10,14 @@ function Grid() {
     //     fetch('/api/products')
     //         .then(response => {
     //             if (!response.ok) {
+    //                 console.error('Server error:', response.status);
     //                 throw new Error("Network response was not ok");
     //             }
     //             return response.json();
     //         })
     //         .then(data => setFiles(data.files))
     //         .catch(error => {
-    //             setError(error.message);
+    //             setError(error.messsage);
     //             console.error("Fetch error:", error);
     //         });
     // }, []);
@@ -25,43 +26,11 @@ function Grid() {
     //     return <div id='gridDiv'>Error: {error}</div>;
     // }
 
-    // retrieve products from user?
-    // const products = [
-    //     "Item 1",
-    //     "Item 2",
-    //     "Item 3",
-    //     "Item 4",
-    //     "Item 5"
-    // ]
-
-    // console.log(files);
-
     // return (
     //     <div id='gridDiv'>
-    //         {/* {products.map((product, index) => (
-    //             <Card index={index} name={product}></Card>
-    //         ))} */}
-
-    //         <ul>
-    //             <p>List</p>
-    //             {files.map((file, index) => (
-    //                 <li key={index}>{file}</li>
-    //             ))}
-    //         </ul>
+    //         {files}
     //     </div>
     // );
-
-    // TESTING
-
-    // const [message, setMessage] = useState('');
-    
-    // useEffect(() => {
-    //     fetch('/api/products')
-    //         .then((res) => res.json())
-    //         .then((data) => setMessage(data.message));
-    // }, []);
-
-    // return <div id='gridDiv'>{message}</div>;
 
     return <div id='gridDiv'>WIP</div>
 }
