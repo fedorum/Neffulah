a594f0751bb821eca7e444aafee2a6df3d3d5597
fedorum <isaaccheah@outlook.com>
2026-01-02 23:39:35 +0800

    Added function to read in file names as filter categories

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,6 +1,55 @@
+import { useState, useEffect } from 'react';
 import '../App.css';
 
-function Search() {
+function Filter() {
+    const [files, setFiles] = useState([]);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        fetch('./api/server')
+            .then(response => {
+                if (!response.ok) {
+                    console.error('Server error:', response.status);
+                    throw new Error("Network response was not ok");
+                }
+                return response.json();
+            })
+            .then(data => setFiles(data.files))
+            .catch(error => {
+                setError(error.messsage);
+                console.error("Fetch error:", error);
+            });
+    }, []);
+
+    if (error) {
+        return <div id='gridDiv'>Error: {error}</div>;
+    }
+
+    // capitalises the first letter of each word in the theme for aesthetics
+    function capitalise(string: string) {
+        const words = string.split(" ");
+
+        // iterates through the 'words' array and capitalises the first letter of each word
+        const capitalisedString = words.map(word => {
+            if (word.length === 0) {
+                return "";
+            }
+            return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+        });
+
+    // joins the map of capitalised words together to form a single string
+    return capitalisedString.join(" ");
+    }
+
+    return (
+        <div id='filterDiv'>
+            <p id='filter'>Filter</p>
+            {files.map((file, index) => (
+                <p className='category' key={index}>{capitalise(file)}</p>
+            ))}
+        </div>
+    );
+
     return (
         <div id='filterDiv'>
             <p id='filter'>Filter</p>
@@ -16,4 +65,4 @@ function Search() {
     );
 }
 
-export default Search;
+export default Filter;
--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -3,34 +3,7 @@ import '../App.css';
 import Card from './Card';
 
 function Grid() {
-    // const [files, setFiles] = useState([]);
-    // const [error, setError] = useState(null);
-
-    // useEffect(() => {
-    //     fetch('/api/products')
-    //         .then(response => {
-    //             if (!response.ok) {
-    //                 console.error('Server error:', response.status);
-    //                 throw new Error("Network response was not ok");
-    //             }
-    //             return response.json();
-    //         })
-    //         .then(data => setFiles(data.files))
-    //         .catch(error => {
-    //             setError(error.messsage);
-    //             console.error("Fetch error:", error);
-    //         });
-    // }, []);
-
-    // if (error) {
-    //     return <div id='gridDiv'>Error: {error}</div>;
-    // }
-
-    // return (
-    //     <div id='gridDiv'>
-    //         {files}
-    //     </div>
-    // );
+    // GET IMAGES OF PRODUCTS
 
     return <div id='gridDiv'>WIP</div>
 }
